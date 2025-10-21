ab75bd6c9ddebe3982c07e5a8c06c02b51f3018b
blackvccat <1293720759@qq.com>
2025-10-21 12:55:23 +0800

    Add GitHub Pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/siman-web3-nft/', // GitHub Pages 部署路径
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
